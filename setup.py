f167d788eb333639b290269da13b0ae5be0440f9
Vikas <palekarprint@gmail.com>
2025-05-31 10:00:03 +0530

    folder structure commit

--- setup.py
+++ setup.py
@@ -17,10 +17,10 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='mlproject',
+name='ML_proj3',
 version='0.0.1',
-author='Krish',
-author_email='krishnaik06@gmail.com',
+author='Vikas Palekar',
+author_email='vikas.palekar@bitwardha.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
